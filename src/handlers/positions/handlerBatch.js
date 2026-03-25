174ff4d5ebbf4b8b044457ebe4f728a505bdd57d
SithuKyaw-Thitsaworks <sithu.kyaw@thitsaworks.com>
2026-03-26 05:19:50 +0700

    lib-419 fix implementation

--- src/handlers/positions/handlerBatch.js
+++ src/handlers/positions/handlerBatch.js
@@ -51,6 +51,12 @@ const { BATCHING } = require('../../shared/constants')
 const consumerCommit = true
 const rethrow = require('../../shared/rethrow')
 
+const _isDeadlockError = (err) =>
+  err?.code === 'ER_LOCK_DEADLOCK' ||
+  err?.errno === 1213 ||
+  err?.message?.includes('ER_LOCK_DEADLOCK') ||
+  (typeof err?.cause === 'string' && err.cause.includes('ER_LOCK_DEADLOCK'))
+
 /**
  * @function positions
  *
@@ -131,12 +137,30 @@ const positions = batchConfig => async (error, messages) => {
   }))
 
   // Start DB Transaction if there are any bins to process
-  const trx = !!Object.keys(bins).length && await BatchPositionModel.startDbTransaction()
+  let trx = !!Object.keys(bins).length && await BatchPositionModel.startDbTransaction()
 
   try {
     if (trx) {
       // Call Bin Processor with the list of account-bins and trx
-      const result = await BinProcessor.processBins(bins, trx)
+      // Retry on deadlock: MySQL recommends retrying the entire transaction on ER_LOCK_DEADLOCK
+      let result
+      let retryCount = 0
+      while (true) {
+        try {
+          result = await BinProcessor.processBins(bins, trx)
+          break
+        } catch (err) {
+          await trx.rollback()
+          trx = null
+          if (_isDeadlockError(err) && retryCount < Config.POSITION_BATCH_DEADLOCK_RETRIES) {
+            retryCount++
+            await new Promise(resolve => setTimeout(resolve, Config.POSITION_BATCH_DEADLOCK_RETRY_DELAY_MS * retryCount))
+            trx = await BatchPositionModel.startDbTransaction()
+          } else {
+            throw err
+          }
+        }
+      }
 
       // If Bin Processor processed bins successfully, commit Kafka offset
       // Commit the offset of last message in the array
